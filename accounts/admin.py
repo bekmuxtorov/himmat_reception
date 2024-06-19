d31e8b57782b016cf2e659e45d3b5610492e88db
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-06-19 22:12:50 +0500

    ➕ Added admin for CustomUser

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'phone_number',
+        'full_name',
+        'gender',
+        'is_staff'
+    )
+    list_filter = ('gender', 'is_staff')
+    ordering = ('created_at',)
+    list_per_page = 50
+    search_fields = ('phone_number', 'full_name', 'username', 'telegram_id')
